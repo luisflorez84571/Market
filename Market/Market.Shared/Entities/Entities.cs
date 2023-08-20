a258189f08e01e2621fac4d27431950e8d545280
LUIS FERNANDO FLOREZ GONZALEZ <luisflorez84571@correo.itm.edu.co>
2023-08-20 18:49:20 -0500

    Creamos Entities Country

---
+++ Market/Market.Shared/Entities/Entities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class Country
+	{
+		public int Id{get; set;}
+		[Display (Name="Pais")]
+		[MaxLength(100)]
+		[Required(ErrorMessage = "El campo {0} es obligatorio.")]
+		public string Name { get; set; } = null;
+		
+	}
+}
